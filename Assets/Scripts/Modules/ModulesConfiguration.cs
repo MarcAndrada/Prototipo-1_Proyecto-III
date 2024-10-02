177cb61d961b7bec452d4b99443ea4aacaa031bc
Crami Enti <marc.andrada@enti.cat>
2024-10-02 12:57:16 +0200

    Module System done

--- Assets/Scripts/Modules/ModulesConfiguration.cs
+++ Assets/Scripts/Modules/ModulesConfiguration.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ModulesConfiguration", menuName = "Scriptable Objects/Modules Configuration")]
 public class ModulesConfiguration : ScriptableObject
 {
-    [field: SerializeField]
-    public int width {  get; private set; }
-    [field: SerializeField]
-    public int height { get; private set; }
+    [field: SerializeField, Tooltip("Cantidad de modulos que habra a lo ancho")]
+    public int Width {  get; private set; }
+    [field: SerializeField, Tooltip("Cantidad de modulos que habra a lo alto")]
+    public int Height { get; private set; }
 
-    [Space, SerializedDictionary("Position", "Object")]
-    public SerializedDictionary<Vector2Int, GameObject> modulesPositions;
+    [field: SerializeField, Tooltip("Este sera la distancia entre cada modulo, sera igual tanto por X como por Y")]
+    public float ModuleOffset { get; private set; }
+
+    [Space, SerializedDictionary("Position", "Object"), Tooltip("Esto relaciona la posicion del modulo con el objeto que habra en la casilla")]
+    public SerializedDictionary<Vector2Int, GameObject> ModulesPositions;
 
 }
--- Assets/Scripts/Modules/ModulesManager.cs
+++ Assets/Scripts/Modules/ModulesManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ModulesManager : MonoBehaviour
@@ -6,14 +7,109 @@ public class ModulesManager : MonoBehaviour
     [SerializeField]
     private ModulesConfiguration configuration;
 
+    [SerializeField]
+    private GameObject moduleBasePrefab;
+    [SerializeField]
+    private GameObject moduleWall;
+    private List<List<Module>> modules;
+    private List<List<GameObject>> modulesObjects;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadModules();
+        LoadModulesObjects();
+        LoadWalls();
     }
 
     private void LoadModules()
     {
+        modules = new List<List<Module>>();
+        modulesObjects = new List<List<GameObject>>();
+
+        Vector3 modulePosition = Vector3.zero;
+        for (int i = 0; i < configuration.Height; i++)
+        {
+            modules.Add(new List<Module>());
+            modulesObjects.Add(new List<GameObject>());
+            modulePosition.x = 0;
+            for (int j = 0; j < configuration.Width; j++)
+            {
+                modules[i].Add(Instantiate(moduleBasePrefab, modulePosition, Quaternion.identity).GetComponent<Module>());
+                modulesObjects[i].Add(null);
+
+                modulePosition.x += configuration.ModuleOffset;
+            }
+
+            modulePosition.z += configuration.ModuleOffset;
+        }
+    }
+    private void LoadModulesObjects()
+    {
+        foreach (KeyValuePair<Vector2Int, GameObject> modulePos in configuration.ModulesPositions) 
+        {
+            GameObject moduleObject = Instantiate(modulePos.Value, modules[modulePos.Key.y][modulePos.Key.x].transform.position, Quaternion.identity);  
+            modulesObjects[modulePos.Key.y][modulePos.Key.x] = moduleObject;
+        }
+    }
+    private void LoadWalls()
+    {
+        Vector3 moduleScale = modules[0][0].transform.localScale;
+        // For para comprobar las paredes superiores y inferiores
+        for (int i = 0; i < modulesObjects.Count; i++)
+        {
+            if (i == 0){
+                //Estan en la parte de abajo
+                for (int j = 0; j < modulesObjects[i].Count; j++)
+                {
+                    CreateWall(0, -1, moduleScale, modules[i][j].transform.position, Vector3.forward);
+                }
+            }
+            else if (i >= modulesObjects.Count - 1)
+            {
+                //Estan en la parte de arriba
+                for (int j = 0; j < modulesObjects[i].Count; j++)
+                {
+                    CreateWall(0, 1, moduleScale, modules[i][j].transform.position, -Vector3.forward);
+                }
+
+            }
+
+
+
+            // For para comprobar las paredes laterales
+            for (int j = 0; j < modulesObjects[i].Count; j++)
+            {
+                if (j == 0)
+                {
+                    //Esta a la izquierda
+                    CreateWall(-1, 0, moduleScale, modules[i][j].transform.position, Vector3.right);
+                }
+                else if (j >= modulesObjects[i].Count - 1)
+                {
+                    //Esta a la derecha
+                    CreateWall(1, 0, moduleScale, modules[i][j].transform.position, -Vector3.right);
+                }
+            }
+
+        }
+
+        
+        
+       
 
     }
+    private void CreateWall(float _directionX, float _directionZ, Vector3 _moduleScale, Vector3 _basePosition, Vector3 _lookDirection)
+    {
+        GameObject wall = Instantiate(moduleWall, Vector3.zero, Quaternion.identity);
+
+        float YOffset = (wall.transform.localScale.y / 2 + _moduleScale.y / 2);
+        float XZOffset = configuration.ModuleOffset / 2;
+
+        Vector3 endPosition = _basePosition + new Vector3(XZOffset * _directionX, YOffset, XZOffset * _directionZ);
+
+        wall.transform.position = endPosition;
+        wall.transform.forward = _lookDirection;
+    }
+
 }
