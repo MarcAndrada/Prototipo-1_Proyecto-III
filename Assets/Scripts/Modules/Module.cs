378a6bad7965a12cb6380809c1ee619a0615410e
Crami Enti <marc.andrada@enti.cat>
2024-10-03 12:57:18 +0200

    Module health started

--- Assets/Scripts/Modules/Module.cs
+++ Assets/Scripts/Modules/Module.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Module : MonoBehaviour
 {
+
+    [SerializeField]
+    private float maxHealth;
+    private float health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -15,4 +20,19 @@ public class Module : MonoBehaviour
     {
         
     }
+
+    public void GetDamage(float _damage)
+    {
+        health -= _damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        CheckIfModuleBreak();
+    }
+    private void CheckIfModuleBreak()
+    {
+        if (health > 0)
+            return;
+
+
+    }
+
 }
--- Assets/Scripts/Modules/ModulesManager.cs
+++ Assets/Scripts/Modules/ModulesManager.cs
@@ -12,7 +12,6 @@ public class ModulesManager : MonoBehaviour
     [SerializeField]
     private GameObject moduleWall;
     private List<List<Module>> modules;
-    private List<List<GameObject>> modulesObjects;
 
     // Start is called before the first frame update
     void Start()
@@ -25,18 +24,15 @@ public class ModulesManager : MonoBehaviour
     private void LoadModules()
     {
         modules = new List<List<Module>>();
-        modulesObjects = new List<List<GameObject>>();
 
         Vector3 modulePosition = Vector3.zero;
         for (int i = 0; i < configuration.Height; i++)
         {
             modules.Add(new List<Module>());
-            modulesObjects.Add(new List<GameObject>());
             modulePosition.x = 0;
             for (int j = 0; j < configuration.Width; j++)
             {
                 modules[i].Add(Instantiate(moduleBasePrefab, modulePosition, Quaternion.identity).GetComponent<Module>());
-                modulesObjects[i].Add(null);
 
                 modulePosition.x += configuration.ModuleOffset;
             }
@@ -49,26 +45,25 @@ public class ModulesManager : MonoBehaviour
         foreach (KeyValuePair<Vector2Int, GameObject> modulePos in configuration.ModulesPositions) 
         {
             GameObject moduleObject = Instantiate(modulePos.Value, modules[modulePos.Key.y][modulePos.Key.x].transform.position, Quaternion.identity);  
-            modulesObjects[modulePos.Key.y][modulePos.Key.x] = moduleObject;
         }
     }
     private void LoadWalls()
     {
         Vector3 moduleScale = modules[0][0].transform.localScale;
         // For para comprobar las paredes superiores y inferiores
-        for (int i = 0; i < modulesObjects.Count; i++)
+        for (int i = 0; i < modules.Count; i++)
         {
             if (i == 0){
                 //Estan en la parte de abajo
-                for (int j = 0; j < modulesObjects[i].Count; j++)
+                for (int j = 0; j < modules[i].Count; j++)
                 {
                     CreateWall(0, -1, moduleScale, modules[i][j].transform.position, Vector3.forward);
                 }
             }
-            else if (i >= modulesObjects.Count - 1)
+            else if (i >= modules.Count - 1)
             {
                 //Estan en la parte de arriba
-                for (int j = 0; j < modulesObjects[i].Count; j++)
+                for (int j = 0; j < modules[i].Count; j++)
                 {
                     CreateWall(0, 1, moduleScale, modules[i][j].transform.position, -Vector3.forward);
                 }
@@ -78,14 +73,14 @@ public class ModulesManager : MonoBehaviour
 
 
             // For para comprobar las paredes laterales
-            for (int j = 0; j < modulesObjects[i].Count; j++)
+            for (int j = 0; j < modules[i].Count; j++)
             {
                 if (j == 0)
                 {
                     //Esta a la izquierda
                     CreateWall(-1, 0, moduleScale, modules[i][j].transform.position, Vector3.right);
                 }
-                else if (j >= modulesObjects[i].Count - 1)
+                else if (j >= modules[i].Count - 1)
                 {
                     //Esta a la derecha
                     CreateWall(1, 0, moduleScale, modules[i][j].transform.position, -Vector3.right);
